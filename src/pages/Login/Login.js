d8785de2ad04a62c4fda02aa4892c137fbe281f6
khoanguyennnn <121242100+khoanguyennnn@users.noreply.github.com>
2023-12-08 03:14:12 +0700

    Final design Login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,20 +1,46 @@
+import { useState } from "react";
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
+
 import classNames from "classnames/bind";
 import styles from './Login.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const cx = classNames.bind(styles)
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isShowPassword, setIsShowPassword] = useState(false);
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('content')}>
                 <div className={cx('title')}>Log in</div>
                 <div className={cx('input-holder')}>
                     <div className={cx('text')}>Email</div>
-                    <input className={cx('input-box')} type="text" placeholder="Enter Username" required />
+                    <input
+                        className={cx('input-box')}
+                        type="text"
+                        placeholder="Enter Email"
+                        required
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                     <div className={cx('text')}>Password</div>
-                    <input className={cx('input-box')} type="password" placeholder="Enter Password" required />
+                    <div className={cx('input-psw')}>
+                        <input
+                            className={cx('input-box')}
+                            type={isShowPassword === true ? "text" : "password"}
+                            placeholder="Enter Password"
+                            required
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                        <FontAwesomeIcon className={cx('eye-icon')} icon={isShowPassword === true ? faEye : faEyeSlash} onClick={() => setIsShowPassword(!isShowPassword)} />
+                    </div>
                 </div>
-                <button className={cx('btn-submit')} type="submit">Login</button>
+                <button className={cx('btn-submit', email && password ? 'active' : '')} disabled={email && password ? false : true} type="submit">Login</button>
 
             </div>
         </div>
